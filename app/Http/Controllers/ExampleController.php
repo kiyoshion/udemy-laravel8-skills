b864e54e6df500f9662622e4c068ace251810882
kiyoshion <kiyoshion@gmail.com>
2021-11-10 15:14:54 +0900

    Update settings

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Example;
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function index()
+    {
+        $examples = Example::all();
+        return view('example', [
+            'examples' => $examples
+        ]);
+    }
+}
